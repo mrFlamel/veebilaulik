337044267ffd4f6feda4a3fae0f3d451ddcf0f29
MrFlamel <jakob.luhamets@gmail.com>
2025-07-22 14:36:43 +0300

    Akordid ja autorid

--- tools.js
+++ tools.js
@@ -54,3 +54,71 @@ function randomInt(min, max) { // min and max included
 function findNextMultiplier(start, search){
     return Math.ceil(start / search) * search;
 }
+
+function looLauluInfo(selgitus, andmed, element="i", id="puudub", siht="lauluInfo"){
+    if (andmed == ""){
+        return
+    }
+    var info = document.createElement(element);
+    info.innerHTML = selgitus + andmed + "<br>";
+    if (id != "puudub"){
+        info.id = id
+    }
+    document.getElementById(siht).appendChild(info);
+}
+
+
+function kontrEnharmooniat(helistik, meeleolu, list, newOrder){
+    const major = ['A-duur', 'Bb-duur', "B-duur/Cb-duur", "C-duur", "C#-duur/Db-duur", "D-duur", "Eb-duur", "E-duur", "F-duur", "F#-duur/Gb-duur", "G-duur", "Ab-duur"]
+    const minor = ['A-moll', 'A#-moll/Bb-moll', 'B-moll', 'C-moll', 'C#-moll', 'D-moll', 'D#-moll/Eb-moll', 'E-moll', 'F-moll', 'F#-moll', 'G-moll', 'G#-moll/Ab-moll']
+
+    if (meeleolu.endsWith("duur")){
+        if (helistik==2 || helistik==4 || helistik==9){
+            list.push(newOrder.indexOf(major[helistik]))
+        }
+    } else {
+        if (helistik==1 || helistik==6 || helistik==11){
+            list.push(newOrder.indexOf(minor[helistik]))
+        }
+    }
+    return list;
+
+}
+
+function votmemark(helistik, list){
+    const majorSharp = ['A-duur', "B-duur/Cb-duur", "C-duur", "C#-duur/Db-duur", "D-duur", "E-duur", "F#-duur/Gb-duur", "G-duur"]
+    const minorSharp = ['A#-moll/Bb-moll', 'C#-moll', 'D#-moll/Eb-moll', 'E-moll', 'F#-moll', 'G#-moll/Ab-moll']
+
+    if (list[0].endsWith("duur")){
+        if (majorSharp.includes(list[helistik])){
+            return "#"
+        } else {
+            return "b"
+        }
+    } else {
+        if (minorSharp.includes(list[helistik])){
+            return "b"
+        } else {
+            return "#"
+        }
+    }
+
+}
+
+function getInputsByValue(value)
+{
+    var allInputs = document.getElementsByTagName("option");
+    var results = [];
+    for(var x=0;x<allInputs.length;x++)
+        if(allInputs[x].value == value)
+            results.push(allInputs[x]);
+    return results;
+}
+
+function reverseParts(str) {
+  const parts = str.split('/');
+  if (parts.length !== 2) {
+    throw new Error("Input must contain exactly one '/' character.");
+  }
+  return `${parts[1]}/${parts[0]}`;
+}
